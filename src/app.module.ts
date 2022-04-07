1c359c8f66a48916ebcb22c77b77eaa7716f2e95
Kai Wang <kaiuchihaa@gmail.com>
2022-04-07 00:05:15 -0300

    sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { LivrosController } from './controllers/Livros.controller'
 import { LivrosServices } from './services/Livros.service';
 import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { Livros } from './models/Livros.models';
 @Module({
   imports: [
     SequelizeModule.forRoot({
@@ -17,6 +18,7 @@ import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
       autoLoadModels: true,
       synchronize: true
     }),
+    SequelizeModule.forFeature([Livros])
   ],
 
   controllers: [AppController, LivrosController],
